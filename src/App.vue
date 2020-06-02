ea3c12f84d414bee2293b7b2b8941f68e0492e98
Brian Underwood <brian@brian-underwood.codes>
2020-06-02 21:05:06 +0200

    Basic logic to display a QR code sign generator

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div id="intro">
+      <h1>Guest Wi-Fi Sign Generator</h1>
+
+      <p>Use the below form to create a sign that you can print out and display in our home or office to share your wi-fi password with guests.  They can even use their camera apps to scan the QR code to sign in immediately!</p>
+    </div>
+
+    <form class="pure-form pure-form-stacked">
+      <fieldset>
+        <legend><em>Options (this form won't appear in the final print)</em></legend>
+
+        <div class="pure-g">
+          <div class="pure-u-1 pure-u-md-1-3">
+            <label for="multi-ssid">Network Name</label>
+            <input type="text" v-model="ssid" id="multi-ssid" class="pure-u-23-24">
+          </div>
+          <div class="pure-u-1 pure-u-md-1-3">
+            <label for="multi-password">Password</label>
+            <input type="text" v-model="password" id="multi-password" class="pure-u-23-24">
+          </div>
+          <div class="pure-u-1 pure-u-md-1-3">
+            <label for="multi-security">Security Type</label>
+            <select v-model="security" id="multi-security" class="pure-u-23-24">
+              <option disabled value="">Choose One</option>
+              <option>WPA</option>
+              <option>WEP</option>
+            </select>
+          </div>
+          <div class="pure-u-1 pure-u-md-1-3">
+            <label for="multi-header">Header</label>
+            <input type="text" v-model="header" id="multi-header" class="pure-u-23-24">
+          </div>
+          <div class="pure-u-1 pure-u-md-1-3">
+            <label for="multi-footer">Footer</label>
+            <input type="text" v-model="footer" id="multi-footer" class="pure-u-23-24">
+          </div>
+          <div class="pure-u-1 pure-u-md-1-3">
+            <button v-on:click="print" class="pure-button pure-u-23-24">Print</button>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+    <Sign
+      v-bind:ssid="ssid"
+      v-bind:password="password"
+      v-bind:security="security"
+      v-bind:header="header"
+      v-bind:footer="footer"
+      />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+
+import Sign from './components/Sign.vue'
 
 @Component({
   components: {
-    HelloWorld,
+    Sign,
+  },
+  data () {
+    return({
+      ssid: '',
+      password: '',
+      security: '',
+
+      header: 'Welcome!',
+      footer: '',
+    })
+  },
+  methods: {
+    print (): void { window.print() },
   },
 })
 export default class App extends Vue {}
@@ -26,4 +86,13 @@ export default class App extends Vue {}
   color: #2c3e50;
   margin-top: 60px;
 }
+
+@media print {
+  @page {size: landscape}
+
+  #intro, form {
+    display: none;
+  }
+}
+
 </style>
---
+++ src/components/Sign.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="sign">
+    <img src="../assets/wifi.png" class="wifi-icon">
+
+    <h2>Wi-Fi</h2>
+
+    <h1 class="header">{{header}}</h1>
+
+    <div class="pure-u-1-3 detail">
+      <h2>Network</h2>
+      {{ssid}}
+    </div>
+
+    <div class="pure-u-1-3">
+      <vue-qr v-bind:text="qr_code_value" v-bind:size="350"></vue-qr>
+    </div>
+
+    <div class="pure-u-1-3 detail">
+      <h2>Password</h2>
+      {{password}}
+    </div>
+
+    <h1 class="footer">{{footer}}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+import VueQr from 'vue-qr'
+
+@Component({
+  components: {
+    VueQr,
+  },
+  props: {
+    ssid: String,
+    password: String,
+    security: String,
+
+    header: String,
+    footer: String,
+  },
+  computed: {
+    qr_code_value (): string {
+      return(`WIFI:S:${this.ssid};T:${this.security};P:${this.password};;`)
+    }
+  },
+})
+export default class Sign extends Vue {}
+</script>
+
+<style>
+
+#sign {
+  margin: 0 auto;
+  border: 5px solid #6193FE;
+  border-radius: 30px;
+  height: 768px;
+  width: 1024px;
+}
+
+.wifi-icon {
+  width: 80px;
+  height: 80px;
+}
+
+.header {
+  margin-top: 80px;
+}
+
+.detail {
+  margin-top: 100px;
+}
+
+</style>
+
